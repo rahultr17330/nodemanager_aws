175f7ecf6be57e1aa8e190eb826ef0219cd48e88
rahul <rahultri17330@gmail.com>
2018-04-22 23:45:34 +0530

    abc

--- public/js/nodemanager.js
+++ public/js/nodemanager.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:5000', {
+const socket = io('https://hidden-spire-15882.herokuapp.com', {
   path: '/nodemanager'
 });
   socket.on('basic_stats',function(stats){
@@ -10,9 +10,9 @@ $("#ram").html('<center>'+ Math.round(basic[2])+'%</center>');
 	$("#file").html('<center>'+basic[3]+'</center>');	
   });
 
-socket.emit('get_cpu_usage');
-socket.on('res_cpu_usage',function(data){
+//socket.emit('get_cpu_usage');
+//socket.on('res_cpu_usage',function(data){
 
-	console.log(data);
-});
+//	console.log(data);
+//});
  
